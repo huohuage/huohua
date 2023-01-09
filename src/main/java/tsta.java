c4e78ce2b6650f7385b20ffcffea0d1e2119311a
com_myit <com_myit@126.com>
2023-01-09 19:28:50 +0800

    2citijiao

--- src/main/java/tsta.java
+++ src/main/java/tsta.java
@@ -1,3 +1,7 @@
 public class tsta {
-    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
+
+
+    public static void main(String[] args){
+        System.out.println("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");
+    }
 }
